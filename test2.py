3ef91880e28ab4598b68763cb44394b08850e8bc
begad-land <125161870+begad-land@users.noreply.github.com>
2023-04-27 00:31:19 +0200

    finfished the game using composition

--- test2.py
+++ test2.py
@@ -1,20 +1,24 @@
 import random
 
-
 class Question:
 
-    def __init__(self,Qs) -> None:
+    def __init__(self,Qs,Qs_num) -> None:
         self.Q=Qs
+        self.Qs_num=Qs_num
     
     def display_questions(self):
-        print(self.Q)
+        Q_num=1
+        print(f'\n{self.Qs_num}){self.Q}\n')
+        
         
 
 class Answers:
 
     def __init__(self,As) -> None:
         self.As=As
-
+    
+    def store_answers(self):
+        return self.As
 
 class Choices:
     
@@ -23,7 +27,7 @@ class Choices:
 
     def display_choices(self):
         for choice in self.choices:
-            print(choice)
+            print(f'{choice}\n--------------')
 
 class Clues:
     
@@ -41,8 +45,6 @@ class Game:
         self.choices=choices
         self.answers=answers
         self.clues=clues
-
-    
     
 
 
@@ -53,7 +55,7 @@ Qs_and_As={
                       
                     'what is the most abudnant element in the atmosphere':(('A. Francium(Fr)', 'B. Oxygen(O2)', 'C. Nitrogen(N2)', 'D. Hydrogen(H)' ),'C','its used to put out fires '),
 
-                    'whats the name of the island thats also a continent?':(('A. Australia' , 'B. Austria', 'C.Bora Bora' , 'D. Hawaii'),'A','its knwon for the kangroos'),
+                    'whats the name of the island thats also a continent?':(('A. Australia' , 'B. Austria', 'C. Bora Bora' , 'D. Hawaii'),'A','its knwon for the kangroos'),
 
                     'the second biggest country in the world?':(('A. Canada', 'B. Russia' , 'C. U.S.A' , 'D. India'),'A','they play a lot of hockey'),
 
@@ -67,27 +69,50 @@ Qs_and_As={
 
                     'What is the country that has the second highest population':(('A. India', 'B. U.S.A', 'C. Indonesia' ,'D. China'), 'A','they are asian'),
 
-                    'What is the biggest organ in the human body':(('A.Liver', 'B. Brain', 'C. Skin', 'D. Lungs'),'C','its the first line of defense in your immune system'),
+                    'What is the biggest organ in the human body':(('A. Liver', 'B. Brain', 'C. Skin', 'D. Lungs'),'C','its the first line of defense in your immune system'),
 
-                    'which planet in our solar system is called the "Red planet" ':(('A.Mercury','B.Venus','C.Earth','D.Jupiter'),'A','it neighbors earth')
+                    'which planet in our solar system is called the "Red planet" ':(('A. Mars','B. Venus','C. Earth','D. Jupiter'),'A','it neighbors earth')
                     }
     
     
 
 Qs_displayed=[]
+score=1
 
 while len(Qs_displayed) != 7:
 
     rand=random.choice(list(Qs_and_As.items()))
 
+    
+    if rand in Qs_displayed:
+        continue
+
     Qs_displayed.append(rand)
 
     game=Game(
-        Question(rand[0]),
+        Question(rand[0],len(Qs_displayed)),
         Choices(rand[1][0]),
         Answers(rand[1][1]),
-        Clues(rand[1][2])
+        Clues(rand[1][2]),
             )
+
     game.questions.display_questions()
     game.choices.display_choices()
-    guess=input('pick A/B/C/D (insert h for a hint)').lower()
+    clue=game.clues.store_clues()
+    model_answer=game.answers.store_answers()
+
+    guess=input(f'pick A/B/C/D (insert h for a hint) ').upper()
+
+    if guess=='H':
+        print(f'{clue}')
+        guess=input(f'pick A/B/C/D ').upper()
+    
+    if guess==model_answer:
+        print('correct')
+        score+=1
+
+    else:
+        print(f'wrong\nanswer:{model_answer}')
+    print('--------------------------------')
+
+print(f'score:{score}/{len(Qs_displayed)}\nthanks for playing :)')
